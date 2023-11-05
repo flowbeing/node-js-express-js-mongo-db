cea6a5c9c834702a86571d45634729a6744b6fe9
admin <flowbeing@proton.me>
2023-11-05 23:58:35 +0000

    nodejs bootcamp: event loops & emitters - Nov 5, 2023

---
+++ node-js/how-node-works/understanding-emitters.js
@@ -0,0 +1,38 @@
+const EventEmitter = require("events");
+const http = require("http");
+let server = http.createServer();
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+let myEmitter = new Sales();
+
+myEmitter.on("newSale", () => {
+  console.log("A new sales has been made");
+});
+
+myEmitter.on("discountAllowed", (discountAmount) => {
+  console.log(`A ${discountAmount} discount was allowed`);
+});
+
+myEmitter.emit("newSale");
+myEmitter.emit("discountAllowed", 9);
+
+server.on("request", (req, res) => {
+  console.log("request callback executed");
+  console.log(`request url: ${req.url}`);
+  res.end("responsed");
+});
+
+server.on("request", (req, res) => {
+  console.log("another request");
+  console.log(`request url: ${req.url}`);
+  console.log();
+});
+
+server.listen("1234", "127.0.0.1", () => {
+  console.log("listening at 127.0.0.1:1234");
+});
