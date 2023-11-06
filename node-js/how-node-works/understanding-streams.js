9fac20b7e13babf38783b104b59f8149d242e144
admin <flowbeing@proton.me>
2023-11-06 22:30:23 +0000

    nodejs bootcamp: understanding streams, how node works - Nov 6, 2023

---
+++ node-js/how-node-works/understanding-streams.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const http = require("http");
+
+let server = http.createServer();
+
+server.on("request", (req, res) => {
+  // SOLUTION ONE - reading files asynchronously & without streams
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     res.end(data);
+  //   });
+
+  // SOLUTION TWO - reading files with streams and forwarding with res.write (may lead to back pressure)
+  // Back pressure simply refers to the situation where chunks of data are not sent nearly as fast as they are received
+  //   let readableFileStream = fs.createReadStream("test-file.txt");
+  //   readableFileStream.on("data", (chunk) => {
+  //     res.write(chunk); // an end event will be emitted when the res.write() (a writeable stream) operation's completed
+  //   });
+
+  //   readableFileStream.on("end", () => {
+  //     console.log("File transfer complete.");
+  //   });
+
+  //   readableFileStream.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+
+  // SOLUTION THREE - resolve backpressure; for speed and efficiency
+  let readableFileStream = fs.createReadStream("test-file.txt");
+  readableFileStream.pipe(res);
+});
+
+server.listen(8080, "127.0.0.1", () => {
+  console.log("Server is listening on port 8080");
+});
